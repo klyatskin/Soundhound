d6a0773512fc12dbb58c0b0f8b1e988fd7c8ad8b
Konstantin Klyatskin <klyatskin@gmail.com>
2019-09-26 00:28:16 -0400

    Initial Commit

---
+++ Soundhound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Soundhound
+//
+//  Created by Konstantin Klyatskin on 2019-09-26.
+//  Copyright © 2019 Konstantin Klyatskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
